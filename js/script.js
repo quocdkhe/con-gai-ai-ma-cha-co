32991b93f409dbc28932888b158461ed7fe111da
quocdkhe <146912117+quocdkhe@users.noreply.github.com>
2024-06-23 14:01:07 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+/**
+ * This function will resize proper size for font "L" "ỒN"
+ */
+function changeFontSize() {
+    var getLarger = document.querySelectorAll('.get-larger');
+    for (let element of getLarger) {
+        var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+        var newSize = parseFloat(currentSize) * 1.5;
+        element.style.fontSize = newSize + 'px';
+    }
+
+    var getSmaller = document.querySelectorAll('.get-smaller');
+    for (let element of getSmaller) {
+        var currentSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+        var newSize = parseFloat(currentSize) * 0.5;
+        element.style.fontSize = newSize + 'px';
+    }
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('False').offsetWidth) - 85;
+    var y = Math.random() * (window.innerHeight - document.getElementById('False').offsetHeight) - 48;
+    document.getElementById('False').style.left = `${x}px`;
+    document.getElementById('False').style.top = `${y}px`;
+}
+
+var pressTrueButton = 0;
+function doSpecialThing() {
+    // Ask her first time
+    if (pressTrueButton == 0) {
+        changeFontSize();
+        pressTrueButton++;
+        document.getElementById('message1').innerHTML = "Trả lời mình thành thật thêm lần nữa đi";
+        return
+    }
+    else if (pressTrueButton == 1) {
+        changeFontSize();
+        pressTrueButton++;
+        document.getElementById('message2').innerHTML = "Nè cậu, đọc kĩ dòng chữ ở trên nha, đừng bảo là chữ nhỏ không nhìn thấy ^_^";
+        return;
+    }
+    else {
+        changeFontSize();
+        pressTrueButton++;
+        document.getElementById('True').style.display = 'none';
+    }
+    document.getElementById('message1').style.display = 'none';
+    document.getElementById('gift-message').style.display = 'inline-block';
+    document.getElementById('gift-button').style.display = 'inline-block';
+}
+
+function nextPage() {
+    window.location.href = "best-wishes.html";
+}
+
+function scrollPhoto(element) {
+    element.addEventListener('click', function () {
+        this.style.transform = 'translateY(-40%)';
+    });
+    document.getElementById('instruction').style.display = 'none';
+    
+    setTimeout(function() { 
+        document.getElementById('refresh').style.display = 'block';
+        document.getElementById('bottomButton').style.display = 'inline-block';
+    }, 2100);
+}
+
+function getRandomPhoto() {
+    const numberOfPhotos = 30;
+    const randomIndex = 1 + Math.floor(Math.random() * numberOfPhotos);
+    const selectedPhoto = "cute" + randomIndex +".jpg";
+    document.getElementById('longPhoto').src ="images/mot-chut-cute/" + selectedPhoto;
+
+}
+
